0883f5f2a20f7636087737a46f31058c3a2e30f5
akjayush <ayushjha110@gmail.com>
2021-08-10 17:08:42 +0530

    started using useState function

---
+++ src/tutorial/useStateErrorEx.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+function UseStateErrorEx() {
+    return (
+        <div>
+            <h2>Use state error  example</h2>
+        </div>
+    )
+}
+
+export default UseStateErrorEx
