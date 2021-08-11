2e46b768cf7c1a13a94d0d9cb3e918a54077d1b4
akjayush <ayushjha110@gmail.com>
2021-08-11 11:39:01 +0530

    creating regular counter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 //import UseStateErrorEx from "./tutorial/useStateErrorEx"
 //import UseStateBasic from "./tutorial/UseStateBasic";
 //import UseStateArray from "./tutorial/UseStateArray";
-import UseStateObjects from "./tutorial/UseStateObjects";
-
+//import UseStateObjects from "./tutorial/UseStateObjects";
+import UseStateCounter from "./tutorial/UseStateCounter";
 
 
 
 function App() {
   return ( <div className='container'>
     <h2>Advance react js</h2>
-    <UseStateObjects />
+    <UseStateCounter />
     
     </div>
     
---
+++ src/tutorial/UseStateCounter.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function UseStateCounter() {
+    const [value, setValue] = useState(0);
+    return (
+        <>
+          <section style={{margin: '4rem 0'}}>
+          <h2>A Regular Counter</h2>
+          <h1>{value}</h1>
+          <button className='btn' onClick={() => setValue(value - 1)} >Decrease</button>    
+          <button className='btn' onClick={() => setValue(value + 1) } >Increase</button> 
+          <button className='btn' onClick={() => setValue(0)} >Reset</button> 
+              </section>  
+        </>
+    )
+}
+
+export default UseStateCounter
