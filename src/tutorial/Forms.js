1f67415c51ca82d27195071cdf1e350bce532cbf
akjayush <ayushjha110@gmail.com>
2021-08-11 15:31:04 +0530

    we have done e target

--- src/tutorial/Forms.js
+++ src/tutorial/Forms.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 
 function Forms() {
-    const [firstName, setFirstName ] = useState("");
-    const [email,  setEmail ] = useState("");
+    const [firstName, setFirstName ] = useState(" ");
+    const [email,  setEmail ] = useState(" ");
     const handleSubmit = (e) =>{
         e.preventDefault(); //this is used to prevent default behavior of our browser
         console.log("hello the people of the world")
@@ -13,12 +13,12 @@ function Forms() {
             <form className='form' onSubmit = {handleSubmit}>
                 <div className='form-control'>
                     <lable htmlFor="firstName">Name :</lable>
-                    <input type="text" id="firstName" name='firstname'></input>
+                    <input type="text" id="firstName" name='firstname' value={firstName} onChange={(e)=> setFirstName(e.target.value)}></input>
 
                 </div>
                 <div className='form-control'>
                     <lable htmlFor='email'> Email :</lable>
-                    <input type='text' id='email' name="email"></input>
+                    <input type='text' id='email' name="email" value={email} onChange={(e)=> setEmail(e.target.value)}></input>
 
                 </div>
                 <button type="submit">Submit</button>
