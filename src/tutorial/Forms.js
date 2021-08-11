eefa7fc1f1a52a9dbedf4dfe1e84333df7d880bf
akjayush <ayushjha110@gmail.com>
2021-08-11 17:16:33 +0530

    added unique id and form complete

--- src/tutorial/Forms.js
+++ src/tutorial/Forms.js
@@ -4,14 +4,14 @@ function Forms() {
     const [firstName, setFirstName ] = useState(" ");
     const [email,  setEmail ] = useState(" ");
     const [people, setPeople] = useState([]);
+
     const handleSubmit = (e) =>{
         e.preventDefault(); //this is used to prevent default behavior of our browser
-        //console.log(firstName,email)
         
         if(firstName && email) {
             console.log('submit the value');
-            const person = {firstName, email};
-            //console.log(person);
+            const person = { id: new Date().getTime().toString(),firstName,email};
+            console.log(person);
             setPeople((people) => {
                 return[...people, person];
             });
@@ -42,7 +42,7 @@ function Forms() {
             {people.map((person, index) => {
                 const {id, firstName, email} = person;
                 return(
-                <div className="item">
+                <div className="item" key = {id}>
                     <h4>{firstName}</h4>
                     <p>{email}</p>
                 </div>
