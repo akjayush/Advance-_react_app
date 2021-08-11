f66e0a6ed24eb492287136a86eb591cc6fa67a3c
akjayush <ayushjha110@gmail.com>
2021-08-11 12:22:50 +0530

    we have learned about newstate and pass value

--- src/tutorial/UseStateCounter.js
+++ src/tutorial/UseStateCounter.js
@@ -2,6 +2,14 @@ import React, {useState} from 'react'
 
 function UseStateCounter() {
     const [value, setValue] = useState(0);
+    const ComplexIncrease = () => {
+        setTimeout(()=> {
+        setValue((prevState)=> {
+            return prevState +1;
+        }  )
+            //setValue(value + 5000);
+        }, 5000) //using 2000 for seonds to change value
+    }
     return (
         <>
           <section style={{margin: '4rem 0'}}>
@@ -11,6 +19,12 @@ function UseStateCounter() {
           <button className='btn' onClick={() => setValue(value + 1) } >Increase</button> 
           <button className='btn' onClick={() => setValue(0)} >Reset</button> 
               </section>  
+          <selection style={{margin: '4rem 0'}}>
+              <h2>A more complex Counter</h2>
+              <h1>{value}</h1>
+              <button className='btn' onClick={ComplexIncrease}> Increase later</button>
+
+          </selection>
         </>
     )
 }
